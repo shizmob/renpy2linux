f5e3fad40e1388149637f11c0562cfa999743a70
Shiz <hi@shiz.me>
2016-02-22 02:51:26 +0100

    Refactor a bit, add OSX and Windows support.

--- README.md
+++ README.md
@@ -1,16 +1,30 @@
 renpy2linux
 ===========
 
-A script to convert Ren'Py Windows releases to Linux-compatible ones.
+A script to convert Ren'Py releases to Windows, OSX ánd Linux-compatible ones.
 
 Prerequisites
 -------------
+* POSIX-compatible sh (like bash)
 * wget
 * tar
-* bash
-* python
-* internet access
+* Python 2.x
+* Internet access
 
 License
 -------
-See LICENSE for details.
+```
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                    Version 2, December 2004
+
+ Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+
+ Everyone is permitted to copy and distribute verbatim or modified
+ copies of this license document, and changing it is allowed as long
+ as the name is changed.
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
+```
--- renpy2linux.sh
+++ renpy2linux.sh
@@ -1,70 +1,87 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
+if test $# -eq 0 ; then
+  echo "usage: $0 <game> [renpy_version]"
+  exit 1
+fi
+BASEDIR="$1"
+cd "$BASEDIR"
+BASEDIR=$(pwd)
+
+trap 'cd "$BASEDIR" && rm -rf __tmp' EXIT
+
+
+echo "==> Determining game information..."
 # Read Ren'Py version.
-if [[ $# -ge 1 ]]; then
-    RENPY_VERSION=$1
+if test $# -gt 1 ; then
+    RENPYVER=$2
 else
-    echo "Determining Ren'Py version..."
+    echo "=> Determining Ren'Py version..."
     if [ ! -f renpy/__init__.py ]; then
-        echo "Could not read renpy/__init__.py -- is this a Ren'Py game?"
+        echo "!! Could not read renpy/__init__.py -- is this a Ren'Py game?"
         exit 1
     fi
 
-    RENPY_VERSION=$(python -c 'from __future__ import print_function; from renpy import version_tuple; print(".".join(str(i) for i in version_tuple[0:3]))')
+    RENPYVER=$(python -c 'from renpy import version_tuple; print ".".join(str(i) for i in version_tuple[:3])')
 fi
-echo "Ren'Py version: ${RENPY_VERSION}"
+echo "=> Ren'Py version: ${RENPYVER}"
 
 # Extract game title.
-EXE=$(echo *.py)
-GAME=${EXE%.py}
-echo "Game title: ${GAME}"
+PYEXE=$(echo *.py)
+TITLE=${PYEXE%.py}
+echo "=> Game title: ${TITLE}"
 
 # Flush temporary directory.
 rm -rf __tmp
 mkdir __tmp
 cd __tmp
 
-echo "Downloading Ren'Py SDK..."
+echo "==> Downloading Ren'Py SDK..."
 # Get the appropriate Ren'Py version SDK to supplement missing files.
-FILE="renpy-$RENPY_VERSION-sdk.tar.bz2"
-DIR="renpy-$RENPY_VERSION-sdk"
-URL="http://renpy.org/dl/$RENPY_VERSION/$FILE"
-if ! wget -nv "$URL"; then
-    cd ..
-    rm -rf __tmp
-    echo "Download failed -- aborting."
+SDKFILE="renpy-${RENPYVER}-sdk.tar.bz2"
+SDKURL="http://renpy.org/dl/${RENPYVER}/${SDKFILE}"
+if ! wget -nv "${SDKURL}"; then
+    echo "!! Download failed -- aborting."
     exit 3
 fi
 
-echo "Extracting Ren'Py SDK..."
-if ! tar -xf "$FILE"; then
-    rm "$FILE"
-    cd ..
-    rm -rf __tmp
-    echo "Extraction failed -- aborting."
+echo "==> Extracting Ren'Py SDK..."
+if ! tar -xf "${SDKFILE}"; then
+    echo "!! Extraction failed -- aborting."
     exit 4
 fi
-rm "$FILE"
+rm "${SDKFILE}"
 
-echo "Copying files..."
-# Copy the required Linux files over.
-if [ -d "$DIR/lib/python"* ]; then
-    cp -R "$DIR/lib/python"* ../lib
-fi
-if [ -d "$DIR/lib/linux-x86" ]; then
-    cp -R "$DIR/lib/linux-x86" ../lib
-fi
-if [ -d "$DIR/lib/linux-i686" ]; then
-    cp -R "$DIR/lib/linux-i686" ../lib
-fi
-if [ -d "$DIR/lib/linux-x86_64" ]; then
-    cp -R "$DIR/lib/linux-x86_64" ../lib
+echo "==> Finding SDK directory..."
+for x in "renpy-${RENPYVER}-sdk" "renpy-${RENPYVER}" ; do
+  if test -d "$x" ; then
+    SDKDIR=$x
+    break
+  fi
+done
+if test -z "${SDKDIR}" ; then
+  echo "!! Couldn't find Ren'Py SDK directory -- aborting."
+  exit 5
 fi
 
-# Copy over the launch script file.
-cp "$DIR/renpy.sh" ../"$GAME".sh
+echo "==> Copying files..."
+# Copy the required platform files.
+cd "${SDKDIR}"
+for x in $(echo lib/*) ; do
+  if ! test -e "../../lib/$x" ; then
+    echo "=> $x"
+    cp -R "$x" ../../lib
+  fi
+done
 
-echo "Done!"
-# Remove the temporary directory.
+# Copy over the launch scripts.
+for x in exe sh app ; do
+  if ! test -e "../../${TITLE}.$x" ; then
+    echo "=> ${TITLE}.$x"
+    cp -R renpy.$x ../../"${TITLE}".$x
+  fi
+done
 cd ..
-rm -rf __tmp
+
+echo "\o/ Done!"
